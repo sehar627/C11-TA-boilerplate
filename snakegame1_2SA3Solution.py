c92d5df62e4284d27da9bd123404574bf22186ea
sehar627 <62961629+sehar627@users.noreply.github.com>
2025-01-23 00:47:56 +0530

    Add files via upload

---
+++ snakegame1_2SA3Solution.py
@@ -0,0 +1,29 @@
+import pygame
+
+# Initialize pygame
+pygame.init()
+
+# Set up display
+WINDOW_WIDTH = 600
+WINDOW_HEIGHT = 600
+
+
+
+display = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption("Basic Pygame Window")
+
+# Game loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:  # Detect window close
+            running = False
+
+    # Fill the screen with a white background
+    display.fill((255, 255, 255))
+    
+    pygame.draw.rect(display, (0, 0, 0), (250,250, 20,20))
+    # Update display
+    pygame.display.flip()
+
+pygame.quit()
